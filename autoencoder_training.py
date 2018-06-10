1fc337a49430dbe65fa4d5b2ef405613cdaeb564
Saransh Mittal <saranshmanu@yahoo.co.in>
2018-06-11 00:04:38 +0530

    Trained model added

--- autoencoder_training.py
+++ autoencoder_training.py
@@ -1,7 +1,6 @@
 from keras.layers import Conv2D, MaxPooling2D, UpSampling2D
 from keras.models import Sequential
 from keras.datasets import mnist
-from keras.callbacks import TensorBoard
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -29,18 +28,17 @@ x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
 x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
 
 model.fit(x_train, x_train,
-                epochs=50,
-                batch_size=128,
+                epochs=1,
+                batch_size=1,
                 shuffle=True,
-                validation_data=(x_test, x_test),
-                callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
+                validation_data=(x_test, x_test))
 
 decoded_imgs = model.predict(x_test)
 model.save('autoencoders.h5')
 
 n = 10
 plt.figure(figsize=(20, 4))
-for i in range(n):
+for i in range(1, n):
     # display original
     ax = plt.subplot(2, n, i)
     plt.imshow(x_test[i].reshape(28, 28))
